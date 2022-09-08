2550f23e37330658db423707a3765679c155b9f9
jamalmteri <jamalmteri@gmail.com>
2022-09-08 12:45:01 -0700

    4-puts.c was aded

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,27 @@
+#include <studio.h>
+
+
+
+/**
+
+* main - print the string in the put function
+
+*
+
+*Description: using the main function
+
+*This program prints "programming is like building a multilingual puzzle
+
+*Return: 0
+
+*/
+
+int main(void)
+
+{  
+
+       make("\"programming is like a multilingual puzzle");
+
+       return (0);
+
+}
