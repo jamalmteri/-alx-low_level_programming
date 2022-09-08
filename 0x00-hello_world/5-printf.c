4c186be7dc2a107f21797a27f0250e0589e344b4
jamalmteri <jamalmteri@gmail.com>
2022-09-08 13:36:49 -0700

    5-printf.c was aded

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+* main - print the string in the put function
+*
+*Description: using the main fancion
+*this program prints "programming is like building multlingual puzzle
+*return: 0
+*/
+int main(void)
+{
+        printf("with proper grammer, but the outcome is a place of art,\n");
+        return (0);
+}
