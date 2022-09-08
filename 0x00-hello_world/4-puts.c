6c847eda3f0968e6fa66e951074a24b61eb8cc8e
jamalmteri <jamalmteri@gmail.com>
2022-09-08 13:15:02 -0700

    4-puts.c was aded

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,6 @@
  *
  * main - entry point
  *
- * 
- *
  * Return: 0 after printings the function 
  *
  */
@@ -13,5 +11,4 @@ int main(void)
 {puts("\"programming is like building a multilingual puzzle");
         return (0);
 }
-
 
