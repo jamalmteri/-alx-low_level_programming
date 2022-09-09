f13bdd50527ee47707a94b115af193292a2ff562
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-09-09 23:27:20 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,41 @@
-#include <stdlib.h>
-#include <time.h>
+#include <stdint.h>
 #include <stdio.h>
-/**
-* main - print if the number is positive, zero, or negative
-*
-*Description: using the main function
-*this program prints "programming is positive, zero, or negative
-*Return: 0
-*/
-int main(void)
-{
-int n;
+#include <stdlib.h>
+#include <openssl/rand.h>
 
-srand(time(0);
-n = rand() - RAND_MAX / 2;
- /* your code goes there */
-if (n > 0)
-{
-        printf("%i is positive\n", n);
-}
-else if (n ==0)
-{       
-        printf("%i is zero\n", n);
-}
-else if (n < 0)
-{
-        printf("%i is positive\n", n);
+/* Random integer in [0, limit) */
+unsigned int random_uint(unsigned int limit) {
+    union {
+        unsigned int i;
+        unsigned char c[sizeof(unsigned int)];
+    } u;
 
+    do {
+        if (!RAND_bytes(u.c, sizeof(u.c))) {
+            fprintf(stderr, "Can't get random bytes!\n");
+            exit(1);
+        }
+    } while (u.i < (-limit % limit)); /* u.i < (2**size % limit) */
+    return u.i % limit;
 }
-return (0);
+
+/* Random double in [0.0, 1.0) */
+double random_double() {
+    union {
+        uint64_t i;
+           unsigned char c[sizeof(uint64_t)];
+    } u;
+
+    if (!RAND_bytes(u.c, sizeof(u.c))) {
+        fprintf(stderr, "Can't get random bytes!\n");
+        exit(1);
+    }
+    /* 53 bits / 2**53 */
+    return (u.i >> 11) * (1.0/9007199254740992.0);
+}
+
+int main() {
+    printf("Dice: %d\n", (int)(random_uint(6) + 1));
+    printf("Double: %f\n", random_double());
+    return 0;
 }
