cd6840d5952f16611c839dc9439ec0a3d074164e
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-09-10 05:02:44 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,18 @@
-#include <
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
