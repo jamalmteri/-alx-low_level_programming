297307cf1795917e42044484dff89ca4d0278aa0
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-09-10 04:54:44 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,21 @@
-#include <
+#include <stdio.h>
+/**
+ * main - Entry point starts
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
