6df39d5bf43b75bc8641839bd608d5f29e2b844d
jamalmteri <jamalmteri@gmail.com>
2022-09-09 18:26:35 -0700

    The last Digits wa Aded

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,56 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.#include <stdint.h>
+
 #include <stdio.h>
+
+#include <stdlib.h>
+
+#include <openssl/rand.h>
+
+
+
+/* Random integer in [0, limit) */
+
+unsigned int random_uint(unsigned int limit) {
+	
+	    union {
+		    
+		            unsigned int i;
+			    
+			            unsigned char c[sizeof(unsigned int)];
+				    
+				        } u;
+	    
+	    
+	    
+	        do {
+			
+			        if (!RAND_bytes(u.c, sizeof(u.c))) {
+					
+					            fprintf(stderr, "Can't get random bytes!\n");
+						    
+						                exit(1);
+								
+								        }
+				
+				    } while (u.i < (-limit % limit)); /* u.i < (2**size % limit) */
+		
+		    return u.i % limit;
+		
+}
+
+
+
+/* Random double in [0.0, 1.0) */
+
+double random_double() {
+	
+	    union {
+		    
+		            uint64_t i;
+			    
+			            unsigned char c[sizeof(uint64_t)];h>
 /**
 * main - print if the number is positive, zero, or negative
 *
