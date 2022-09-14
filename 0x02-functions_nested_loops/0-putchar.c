f6092614f1f38c4ae8be432191daeb4c500ee225
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-09-14 03:48:24 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,19 @@
-done
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Entry point print the characters
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('\n');
+	return (0);
+}
