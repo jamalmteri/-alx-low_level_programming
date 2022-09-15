1906d5c90400fe259f4249e9a42baaf0b35b2a95
jamalmteri <jamalmteri@gmail.com>
2022-09-15 08:37:24 -0700

    New updates

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,14 +24,4 @@ else
 largest = c;
 }
 return (largest);
-=======
-        int a, b, c;
-        int largest;
-        a = 972;
-        b = -98;
-        c = 0;
-        largest = largest_number(a, b, c);
-        printf("%d is the largest number\n", largest);
-        return (0);
->>>>>>> 29befd7144e9b7cbc8620eeb4b32f563e99a7f78
 }
