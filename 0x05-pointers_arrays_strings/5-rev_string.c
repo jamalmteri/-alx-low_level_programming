a8573e4862dfaf1076678dfeb7ef215899a12971
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-09-19 22:08:59 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,29 +1,25 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * rev_string - check the code for Holberton School students.
- * @s: pointer int type
- * Return: Always 0.
+ * rev_string - Entry point
+ * Desc: Entry
+ *@s: pointer
+ * Return: Always 0 (Success)
  */
 void rev_string(char *s)
 {
-int i, j;
-char temp[500];
+	int i;
+	int j;
+	char k;
 
-for (i = 0; *s != '\0'; i++)
-{
-temp[i] = *s;
-s++;
-}
-
-s--;
-
-for (j = 0; j != i; j++)
-{
-*s = temp[j];
-s--;
-}
-
-s++;
-
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+	}
+	i--;
+	for (j = 0 ; j < i ; j++, i--)
+	{
+	k = *(s + i);
+	*(s + i) = *(s + j);
+	*(s + j) = k;
+	}
 }
