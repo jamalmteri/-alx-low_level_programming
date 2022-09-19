1d12157b70e818caed2e727a4ab89fcd8f885e73
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-09-19 21:27:42 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * rev_string - a function that reverses a string
  * @s: input to reverse
@@ -9,14 +9,13 @@
 		char rev = s[0];
 		int fcounter = 0;
 		int i;
-
 		while (s[fcounter] != '\0')
 			fcounter++;
-
 		for (i = 0; i < fcounter; i++)
 		{
 			fcounter--;
 			rev = s[i];
 			s[i] = s[fcounter];
 			s[fcounter] = rev;
+		}
 	}
