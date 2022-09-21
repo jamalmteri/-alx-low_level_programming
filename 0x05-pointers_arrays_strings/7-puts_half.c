db5b61c171891f4a2fbee7b56e6a6147a68182e9
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-09-21 05:56:49 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,35 +1,25 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * puts_half - prints a string
- * @str: string to print
- *
- * Return: nothing
+ * puts_half - a function that prints half of a string, followed by a new line.
+ * @str: An input string
+ * Return: Nothing
  */
-
 void puts_half(char *str)
 {
-	int i, len = _strlen(str);
+	int len = 0, i, n;
 
-	for (i = ((len - 1) / 2) + 1; i < len; i++)
-		putchar(*(str + i));
-	putchar(10);
-}
-
-/**
-* _strlen - returns the length of a string
-* @s: string
-*
-* Return: the length of the given string
-*/
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*(s + len) != '\0')
+	while (str[len] != '\0')
 		len++;
 
-	return (len);
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
