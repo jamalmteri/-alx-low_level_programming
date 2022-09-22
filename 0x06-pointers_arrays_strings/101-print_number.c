b140fa78c29f29ce089f030cbaeb5f3f0341bd97
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-09-22 12:51:45 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,28 @@
-#include <stdio.h>
 
-int main(void)
-{
-  int n;
-  int a[5];
-  int *p;
-
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+#include "main.h"                                                                                                                     
+                                                                                                                                      
+/**                                                                                                                                   
+ * leet - Encodes a string to 1337.                                                                                                   
+ * @str: The string to be encoded.                                                                                                    
+ *                                                                                                                                    
+ * Return: A pointer to the encoded string.                                                                                           
+ */                                                                                                                                   
+char *leet(char *str)                                                                                                                 
+{                                                                                                                                     
+        int indx1 = 0, indx2;                                                                                                         
+        char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};                                                                      
+                                                                                                                                      
+        while (str[indx1])                                                                                                            
+        {                                                                                                                             
+                for (indx2 = 0; indx2 <= 7; indx2++)                                                                                  
+                {                                                                                                                     
+                        if (str[indx1] == leet[indx2] ||                                                                              
+                            str[indx1] - 32 == leet[indx2])                                                                           
+                                str[indx1] = indx2 + '0';                                                                             
+                }                                                                                                                     
+                                                                                                                                      
+                indx1++;                                                                                                              
+        }                                                                                                                             
+                                                                                                                                      
+        return (str);                                                                                                                 
 }
