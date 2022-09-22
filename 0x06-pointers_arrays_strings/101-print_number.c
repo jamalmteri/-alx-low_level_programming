5e9f664b6c92fc62873cbd4ac089e6d757e3ae60
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-09-22 12:53:44 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,41 @@
-
 #include "main.h"                                                                                                                     
                                                                                                                                       
 /**                                                                                                                                   
- * leet - Encodes a string to 1337.                                                                                                   
- * @str: The string to be encoded.                                                                                                    
+ * rot13 - Encodes a string using rot13.                                                                                                    
  *                                                                                                                                    
  * Return: A pointer to the encoded string.                                                                                           
- */                                                                                                                                   
-char *leet(char *str)                                                                                                                 
+ */
+char *rot13(char *str)                                                                                                                
 {                                                                                                                                     
         int indx1 = 0, indx2;                                                                                                         
-        char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};                                                                      
+        char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',                                                                            
+                             'G', 'H', 'I', 'J', 'K', 'L',                                                                            
+                             'M', 'N', 'O', 'P', 'Q', 'R',                                                                            
+                             'S', 'T', 'U', 'V', 'W', 'X',                                                                            
+                             'Y', 'Z', 'a', 'b', 'c', 'd',                                                                            
+                             'e', 'f', 'g', 'h', 'i', 'j',                                                                            
+                             'k', 'l', 'm', 'n', 'o', 'p',                                                                            
+                             'q', 'r', 's', 't', 'u', 'v',                                                                            
+                             'w', 'x', 'y', 'z'};                                                                                     
+        char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',                                                                            
+                             'T', 'U', 'V', 'W', 'X', 'Y',                                                                            
+                             'Z', 'A', 'B', 'C', 'D', 'E',                                                                            
+                             'F', 'G', 'H', 'I', 'J', 'K',                                                                            
+                             'L', 'M', 'n', 'o', 'p', 'q',                                                                            
+                             'r', 's', 't', 'u', 'v', 'w',                                                                            
+                             'x', 'y', 'z', 'a', 'b', 'c',                                                                            
+                             'd', 'e', 'f', 'g', 'h', 'i',                                                                            
+                             'j', 'k', 'l', 'm'};                                                                                     
                                                                                                                                       
         while (str[indx1])                                                                                                            
         {                                                                                                                             
-                for (indx2 = 0; indx2 <= 7; indx2++)                                                                                  
+                for (indx2 = 0; indx2 < 52; indx2++)                                                                                  
                 {                                                                                                                     
-                        if (str[indx1] == leet[indx2] ||                                                                              
-                            str[indx1] - 32 == leet[indx2])                                                                           
-                                str[indx1] = indx2 + '0';                                                                             
+                        if (str[indx1] == alphabet[indx2])                                                                            
+                        {                                                                                                             
+                                str[indx1] = rot13key[indx2];                                                                         
+                                break;                                                                                                
+                        }                                                                                                             
                 }                                                                                                                     
                                                                                                                                       
                 indx1++;                                                                                                              
