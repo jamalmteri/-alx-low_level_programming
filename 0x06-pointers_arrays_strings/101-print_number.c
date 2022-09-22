1222c0da454014fe450ee78c04cf83ee6048caab
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-09-22 13:17:55 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -16,8 +16,8 @@ void print_number(int n)
   
         x = n; 
                                                                                                                                       
-        if (x /10) > 0)                                                                                                           
-                print_number(num / 10);                                                                                               
+        if (x /10)                                                                                                          
+                print_number(x / 10);                                                                                               
                                                                                                                                       
         _putchar(x % 10) + '0');                                                                                                   
 }
