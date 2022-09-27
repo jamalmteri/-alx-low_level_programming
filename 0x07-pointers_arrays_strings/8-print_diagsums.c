9bab3fc976e5521acee5873094e831b51d2e15f8
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-09-27 03:17:44 +0300

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,19 @@
-tyt
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, n, jumla1 = 0, jumla2 = 0;
+
+	for (i = 0; i <= (size * size); i = i + size + 1)
+		jumla1 = jumla1 + a[i];
+
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+		jumla2 = jumla2 + a[n];
+	printf("%d, %d\n", jumla1, jumla2);
+}
