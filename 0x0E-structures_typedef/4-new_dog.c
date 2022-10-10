035f7dab799f46d0f72f881143fe8b6f4a16afef
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-10-10 14:13:48 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 
+
 /**
 *_strlen - returns length of
 *a string
@@ -14,6 +15,7 @@ len++;
 
 return (len);
 }
+
 
 /**
 *_strcopy - copy string pointed by src
@@ -31,4 +33,48 @@ dest[index] = src[index];
 
 dest[index] = '\0';
 return (dest);
+}
+
+
+
+
+/**
+*new_dog - creates a new dog
+*@name: name of new dog
+*@age: age of new dog
+*@owner: owner of new dog
+*Return: returns NULL in case
+*of failure
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *doggo;
+
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
+
+doggo = malloc(sizeof(dog_t));
+if (doggo == NULL)
+return (NULL);
+
+doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+if (doggo->name == NULL)
+{
+free(doggo);
+return (NULL);
+}
+
+doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+if (doggo->owner == NULL)
+{
+free(doggo->name);
+free(doggo);
+return (NULL);
+}
+
+doggo->name = _strcopy(doggo->name, name);
+doggo->age = age;
+doggo->owner = _strcopy(doggo->owner, owner);
+
+return (doggo);
 }
